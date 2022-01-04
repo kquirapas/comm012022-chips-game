27d74aec5a55345832444c7beb139e9f16ed6a4b
kmquirapas <kmquirapas@gmail.com>
2022-01-05 02:30:13 +0800

    added classes, inventory system, key and door system

--- README.md
+++ README.md
@@ -1,36 +1,39 @@
 # Chips Game Spin-Off
 
-# Requirements Checklist
+## Requirements Checklist
 
-## Maps
+### Maps
 - [x] Maps are at least 20rows by 40cols
 - [x] Load Maps from file
 - [ ] 4 maps (1 start map, 3 other maps)
 
-## Tiles
+### Tiles
 - [x] Starting Tile 
 - [x] Passable Tile 
 - [x] Impassable / Wall Tile
-- [ ] Locked / Door Tile (Green and Yellow, checks for key in inventory to open)
+- [x] Locked / Door Tile (Green and Yellow, checks for key in inventory to open)
 - [ ] Element Tile (Fire and Water)
 - [ ] Thief Tile (reset to start point, discard all inventory)
 - [ ] Slide Tile (pushes the player in that direction, not moving)
 - [ ] Exit Tile
 
 
-## Items
-- [ ] Keys (Green and Yellow)
+### Items
+- [x] Keys (Green and Yellow)
 - [ ] Element Immunity Item (Fire and Water, max 1, current gets replaced when acquiring new one)
 
-## Player
-- [ ] show inventory on screen (update inventory on item pickup)
-- [ ] display chips requirement on screen
-- [ ] display chips gathered on screen
-- [ ] display current immunity on screen
-- [x] move with WASD across map
+### Player
+- [x] Show inventory on screen (update inventory on item pickup)
+- [ ] Display chips requirement on screen
+- [ ] Display chips gathered on screen
+- [ ] Display current immunity on screen
+- [x] Move with WASD across map
 
-## Bonus
-- [ ] splash screen (press space to start)
-- [ ] sound design (bg music and sfx)
-- [ ] enemies
-- [ ] press esc to go back to splash screen (restart)
+### Bonus
+- [ ] Splash screen (press space to start)
+- [ ] Sound design (bg music and sfx)
+- [ ] Enemies
+- [ ] Press esc to go back to splash screen (restart)
+
+### Progress
+9 / 22 (41% Done)
--- main.py
+++ main.py
@@ -13,8 +13,13 @@ WHITE = (255, 255, 255)
 FLOOR = 0
 WALL = 1
 PLAYER = 2
-DOOR = 3
-IMPASSABLE = [WALL, DOOR]
+GREENDOOR = 3
+YELLOWDOOR = 4
+GREENKEY = 5
+YELLOWKEY = 6
+
+DOORS = [GREENDOOR, YELLOWDOOR]
+ITEMS = [GREENKEY, YELLOWKEY]
 
 # DIRECTIONS
 UP = (0, -1)
@@ -31,22 +36,20 @@ SCREENHEIGHT = SCREENWIDTH
 VISIBLEBLOCKS = 9
 HALFVIEW = math.floor(VISIBLEBLOCKS / 2)
 BLOCKSIZE = SCREENWIDTH / VISIBLEBLOCKS
+INVENTORYSIZE = 0.8 * BLOCKSIZE;
+INVENTORYMARGIN = int((BLOCKSIZE - INVENTORYSIZE) / 2)
 
 def main():
     # file handling
     # loading initial map data from file into variables
-    map_file = open("assets/map1.csv")
+    player = Player()
 
-    curr_line = map_file.readline()
-    player_location = tuple([int(x) for x in curr_line[0:-1].split(",")])
+    map0 = Map("assets/map0.csv")
+    map1 = Map("assets/map1.csv")
 
-    curr_map = []
+    curr_level = map1
+    curr_level.load(player)
     
-    curr_line = map_file.readline()
-    while curr_line:
-        curr_map.append([int(cell) for cell in curr_line[0:-1].split(",")])
-        curr_line = map_file.readline()
-
     # initialize pygame
     pygame.init()
 
@@ -67,6 +70,18 @@ def main():
     map_assets[WALL] = pygame.image.load("assets/wall.jpeg")
     map_assets[WALL] = pygame.transform.scale(map_assets[WALL], (BLOCKSIZE, BLOCKSIZE))
 
+    map_assets[GREENDOOR] = pygame.image.load("assets/greendoor.png")
+    map_assets[GREENDOOR] = pygame.transform.scale(map_assets[GREENDOOR], (BLOCKSIZE, BLOCKSIZE))
+
+    map_assets[YELLOWDOOR] = pygame.image.load("assets/yellowdoor.png")
+    map_assets[YELLOWDOOR] = pygame.transform.scale(map_assets[YELLOWDOOR], (BLOCKSIZE, BLOCKSIZE))
+
+    map_assets[GREENKEY] = pygame.image.load("assets/greenkey.jpeg")
+    map_assets[GREENKEY] = pygame.transform.scale(map_assets[GREENKEY], (BLOCKSIZE, BLOCKSIZE))
+
+    map_assets[YELLOWKEY] = pygame.image.load("assets/yellowkey.jpeg")
+    map_assets[YELLOWKEY] = pygame.transform.scale(map_assets[YELLOWKEY], (BLOCKSIZE, BLOCKSIZE))
+
     # game loop
     while True:
         for event in pygame.event.get():
@@ -74,30 +89,31 @@ def main():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    if isPassable(player_location, curr_map, UP):
-                        player_location = playerMoveUp(player_location, curr_map)
+                    if checkNextTile(player, curr_level.map, UP):
+                        player.location = player.moveUp(player.location, curr_level.map)
                 elif event.key == pygame.K_a:
-                    if isPassable(player_location, curr_map, LEFT):
-                        player_location = playerMoveLeft(player_location, curr_map)
+                    if checkNextTile(player, curr_level.map, LEFT):
+                        player.location = player.moveLeft(player.location, curr_level.map)
                 elif event.key == pygame.K_s:
-                    if isPassable(player_location, curr_map, DOWN):
-                        player_location = playerMoveDown(player_location, curr_map)
+                    if checkNextTile(player, curr_level.map, DOWN):
+                        player.location = player.moveDown(player.location, curr_level.map)
                 elif event.key == pygame.K_d:
-                    if isPassable(player_location, curr_map, RIGHT):
-                        player_location = playerMoveRight(player_location, curr_map)
+                    if checkNextTile(player, curr_level.map, RIGHT):
+                        player.location = player.moveRight(player.location, curr_level.map)
+
 
         # add standard game bg color
         screen.fill(BLACK)
 
         # find viewport bounds based on current player location and HALFVIEW
         # maintains player centered in viewport (unless edge is reached)
-        col_start = player_location[0] - HALFVIEW
-        row_start = player_location[1] - HALFVIEW
+        col_start = player.location[0] - HALFVIEW
+        row_start = player.location[1] - HALFVIEW
         if col_start < 0:
             col_start = 0
             col_end = VISIBLEBLOCKS - 1
         else:
-            col_end = player_location[0] + HALFVIEW
+            col_end = player.location[0] + HALFVIEW
             if col_end >= MAPWIDTH:
                 col_end = MAPWIDTH - 1
                 col_start = col_end - VISIBLEBLOCKS + 1
@@ -106,7 +122,7 @@ def main():
             row_start = 0
             row_end = VISIBLEBLOCKS - 1
         else:
-            row_end = player_location[1] + HALFVIEW
+            row_end = player.location[1] + HALFVIEW
             if row_end >= MAPWIDTH:
                 row_end = MAPWIDTH - 1
                 row_start = row_end - VISIBLEBLOCKS + 1
@@ -116,12 +132,18 @@ def main():
         # then display respective tile image from map_assets dict with screen.blit
         for row in range(row_start, row_end + 1):
             for col in range(col_start, col_end + 1):
-                screen.blit(map_assets[curr_map[row][col]], ((col - col_start) * BLOCKSIZE, (row - row_start) * BLOCKSIZE))
+                screen.blit(map_assets[curr_level.map[row][col]], ((col - col_start) * BLOCKSIZE, (row - row_start) * BLOCKSIZE))
+
+        # display UI overlay
+        # inventory
+        for index in range(len(player.inventory)):
+            screen.blit(pygame.transform.scale(map_assets[player.inventory[index]], (INVENTORYSIZE, INVENTORYSIZE)), (index * BLOCKSIZE + INVENTORYMARGIN, SCREENHEIGHT - BLOCKSIZE + INVENTORYMARGIN))
 
         # update display with pygame
         pygame.display.flip()
 
 
+# function definitions
 def printMap(pMap):
     '''
     hatdog
@@ -133,38 +155,91 @@ def printMap(pMap):
             print(f"{col}", end="")
         print()
 
-def isPassable(pLoc, pMap, pDir):
-    return not (pMap[pLoc[1] + pDir[1]][pLoc[0] + pDir[0]] in IMPASSABLE)
+def checkNextTile(pPlayer, pMap, pDir):
+    # check next location for enemies, items, etc.
+    # updates map and player (inventory) accordingly
+    next_tile = pMap[pPlayer.location[1] + pDir[1]][pPlayer.location[0] + pDir[0]]
+
+    if next_tile == WALL:
+        return False
+    elif next_tile in DOORS:
+        if next_tile == GREENDOOR and GREENKEY in pPlayer.inventory:
+            pPlayer.removeItem(GREENKEY)
+            print(f"inventory: {pPlayer.inventory}")
+            return True
+        elif next_tile == YELLOWDOOR and YELLOWKEY in pPlayer.inventory:
+            pPlayer.removeItem(YELLOWKEY)
+            print(f"inventory: {pPlayer.inventory}")
+            return True
+        else:
+            return False
+    elif next_tile in ITEMS:
+        pPlayer.addItem(next_tile)
+        print(f"inventory: {pPlayer.inventory}")
+        return True
+    else:
+        return True
+
 
 def isDanger(pLoc, pMap, pDir):
     raise NotImplementedError
 
-def playerMoveLeft(pLoc, pMap):
-    new_location = (pLoc[0] - 1, pLoc[1])
-    pMap[pLoc[1]][pLoc[0]] = FLOOR
-    pMap[new_location[1]][new_location[0]] = PLAYER
-    return new_location
 
-def playerMoveRight(pLoc, pMap):
-    new_location = (pLoc[0] + 1, pLoc[1])
-    pMap[pLoc[1]][pLoc[0]] = FLOOR
-    pMap[new_location[1]][new_location[0]] = PLAYER
-    return new_location
+# classes
+class Player:
+    def __init__(self):
+        self.location = (0, 0)
+        self.inventory = []
 
-def playerMoveUp(pLoc, pMap):
-    new_location = (pLoc[0], pLoc[1] - 1)
-    pMap[pLoc[1]][pLoc[0]] = FLOOR
-    pMap[new_location[1]][new_location[0]] = PLAYER
-    return new_location
+    def relocate(self, pLoc):
+        self.location = pLoc
 
-def playerMoveDown(pLoc, pMap):
-    new_location = (pLoc[0], pLoc[1] + 1)
-    pMap[pLoc[1]][pLoc[0]] = FLOOR
-    pMap[new_location[1]][new_location[0]] = PLAYER
-    return new_location
+    def moveLeft(self, pLoc, pMap):
+        new_location = (pLoc[0] - 1, pLoc[1])
+        pMap[pLoc[1]][pLoc[0]] = FLOOR
+        pMap[new_location[1]][new_location[0]] = PLAYER
+        return new_location
 
-def displayMap(pMap):
-    raise NotImplementedError
+    def moveRight(self, pLoc, pMap):
+        new_location = (pLoc[0] + 1, pLoc[1])
+        pMap[pLoc[1]][pLoc[0]] = FLOOR
+        pMap[new_location[1]][new_location[0]] = PLAYER
+        return new_location
+
+    def moveUp(self, pLoc, pMap):
+        new_location = (pLoc[0], pLoc[1] - 1)
+        pMap[pLoc[1]][pLoc[0]] = FLOOR
+        pMap[new_location[1]][new_location[0]] = PLAYER
+        return new_location
+
+    def moveDown(self, pLoc, pMap):
+        new_location = (pLoc[0], pLoc[1] + 1)
+        pMap[pLoc[1]][pLoc[0]] = FLOOR
+        pMap[new_location[1]][new_location[0]] = PLAYER
+        return new_location
+
+    def addItem(self, pItem):
+        self.inventory.append(pItem)
+
+    def removeItem(self, pItem):
+        self.inventory.remove(pItem)
+
+class Map:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.map = []
+
+    def load(self, player):
+        map_file = open(self.filepath)
+        curr_line = map_file.readline()
+
+        player.relocate(tuple([int(x) for x in curr_line[0:-1].split(",")]))
+
+        curr_line = map_file.readline()
+        while curr_line:
+            self.map.append([int(cell) for cell in curr_line[0:-1].split(",")])
+            curr_line = map_file.readline()
+        map_file.close()
 
 if __name__ == "__main__":
     main()
